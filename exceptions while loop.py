c26644bc28119df1d82ad60d55123f8b097a0905
yogesh rai <157304575+Y0geshhrai@users.noreply.github.com>
2025-05-08 00:08:49 +0545

    Add files via upload

---
+++ exceptions while loop.py
@@ -0,0 +1,48 @@
+
+#  using while loop and break(if not break use it loops everytime)
+while True:
+    try:
+        x = int(input("what's the value of x? "))
+    except ValueError:
+        print("x is not an integer")
+    else:
+        break
+print(f"x is {x}")
+
+#or
+while True:
+    try:
+        x = int(input("what's the value of x? "))
+        break
+    except ValueError:
+        print("x is not an integer")
+print(f"x is {x}")
+
+#or
+def main():
+    x = get_int()
+    print(f"x is {x}")
+
+def get_int():
+    while True:
+        try:
+            x = int(input("what's the value of x? "))
+        except ValueError:
+            print("x is not an integer")
+        else:
+            return x
+
+main()
+
+ #or
+def main():
+    x = get_int()
+    print(f"x is {x}")
+def get_int():
+    while True:
+        try:
+          return int(input("what's the value of x? "))
+        except ValueError:
+            print("x is not an integer")
+
+main()
