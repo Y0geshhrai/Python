3994c0543e7bce7403dedf7687f14f197ec03ab2
yogesh rai <157304575+Y0geshhrai@users.noreply.github.com>
2025-05-07 23:02:42 +0545

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,9 @@
+        #allows to associate something with something else
+#variables(students)
+students = {"yogs": "ktm" ,
+            "harry": "ktm",
+            "jay": "bkt",
+}
+for student in students:
+        #sep= separator
+    print(student , students[student], sep=", ")
