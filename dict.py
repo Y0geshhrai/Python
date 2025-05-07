09f96fec837fc154e84b016f7429088fe67e44f0
yogesh rai <157304575+Y0geshhrai@users.noreply.github.com>
2025-05-07 23:03:58 +0545

    Delete dict.py

--- dict.py
+++
@@ -1,9 +0,0 @@
-        #allows to associate something with something else
-#variables(students)
-students = {"yogs": "ktm" ,
-            "harry": "ktm",
-            "jay": "bkt",
-}
-for student in students:
-        #sep= separator
-    print(student , students[student], sep=", ")
